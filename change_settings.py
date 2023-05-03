379d21a460d7d83f03e76e8269cf2fc1524477e0
croutonso <52742396+croutonso@users.noreply.github.com>
2023-05-02 23:27:41 -0700

    Update change_settings.py

--- change_settings.py
+++ change_settings.py
@@ -1,6 +1,7 @@
 import minimalmodbus
 import serial
 import time
+from contextlib import closing
 
 SLAVE_ADDRESS = 0x01
 DEVICE_PORT = '/dev/ttyUSB0'
@@ -11,70 +12,75 @@ DEVICE_STOPBITS = 2
 def connect_modbus_device(port, baudrate, parity, stopbits):
     instrument = minimalmodbus.Instrument(port, SLAVE_ADDRESS)
     instrument.serial.baudrate = baudrate
+    instrument.serial.bytesize = 8
     instrument.serial.parity = parity
     instrument.serial.stopbits = stopbits
+    instrument.serial.timeout = 1
     return instrument
 
-def display_menu():
-    print("Select the parameter to change:")
-    print("1. Set High Voltage Alarm Threshold")
-    print("2. Set Low Voltage Alarm Threshold")
-    print("3. Set Slave Address")
-    print("4. Set Current Range (PZEM-017 only)")
+def read_current_values(instrument):
+    with closing(instrument.serial):
+        time.sleep(1)
+        high_voltage_alarm = instrument.read_register(0x0000, functioncode=4)
+        low_voltage_alarm = instrument.read_register(0x0001, functioncode=4)
+        slave_address = instrument.read_register(0x0002, functioncode=4)
+        current_range = instrument.read_register(0x0003, functioncode=4)
+        return high_voltage_alarm, low_voltage_alarm, slave_address, current_range
+
+def display_menu(current_values):
+    print("\nSelect the parameter to change:")
+    print("1. Set High Voltage Alarm Threshold (Current: {} V, Default: 250 V)".format(current_values[0]))
+    print("2. Set Low Voltage Alarm Threshold (Current: {} V, Default: 0 V)".format(current_values[1]))
+    print("3. Set Slave Address (Current: {}, Default: 1)".format(current_values[2]))
+    print("4. Set Current Range (PZEM-017 only) (Current: {}, Default: 0)".format(current_values[3]))
     print("5. Reset Energy")
     print("6. Exit")
 
 def set_high_voltage_alarm_threshold(instrument):
-    new_threshold = int(input("Enter new High Voltage Alarm Threshold (in 0.01V): "))
-    instrument.write_register(0x0000, new_threshold, functioncode=6)
-    time.sleep(1)
+    value = int(input("Enter the new High Voltage Alarm Threshold (0-300 V): "))
+    with closing(instrument.serial):
+        instrument.write_register(0x0000, value, functioncode=6)
 
 def set_low_voltage_alarm_threshold(instrument):
-    new_threshold = int(input("Enter new Low Voltage Alarm Threshold (in 0.01V): "))
-    instrument.write_register(0x0001, new_threshold, functioncode=6)
-    time.sleep(1)
+    value = int(input("Enter the new Low Voltage Alarm Threshold (0-300 V): "))
+    with closing(instrument.serial):
+        instrument.write_register(0x0001, value, functioncode=6)
 
 def set_slave_address(instrument):
-    new_address = int(input("Enter new Slave Address: "))
-    instrument.write_register(0x0002, new_address, functioncode=6)
-    time.sleep(1)
-    return new_address
+    value = int(input("Enter the new Slave Address (1-247): "))
+    with closing(instrument.serial):
+        instrument.write_register(0x0002, value, functioncode=6)
+    return value
 
 def set_current_range(instrument):
-    print("Current Range Options:")
-    print("0: 100A")
-    print("1: 50A")
-    print("2: 200A")
-    print("3: 300A")
-    new_range = int(input("Enter new Current Range: "))
-    instrument.write_register(0x0003, new_range, functioncode=6)
-    time.sleep(1)
+    value = int(input("Enter the new Current Range (0 for 5 A, 1 for 100 A): "))
+    with closing(instrument.serial):
+        instrument.write_register(0x0003, value, functioncode=6)
 
 def reset_energy(instrument):
-    confirm = input("Are you sure you want to reset energy? (y/n): ")
-    if confirm.lower() == 'y':
-        instrument.write_register(0x42, 0, functioncode=6)
-        time.sleep(1)
-        print("Energy reset successful.")
-    else:
-        print("Energy reset canceled.")
+    with closing(instrument.serial):
+        instrument.write_register(0x0005, 0, functioncode=6)
 
 def main():
     instrument = connect_modbus_device(DEVICE_PORT, DEVICE_BAUDRATE, DEVICE_PARITY, DEVICE_STOPBITS)
     
     while True:
-        display_menu()
+        current_values = read_current_values(instrument)
+        display_menu(current_values)
         user_choice = int(input("Enter your choice: "))
         
         try:
             if user_choice == 1:
                 set_high_voltage_alarm_threshold(instrument)
                 print("High Voltage Alarm Threshold changed successfully.")
+                instrument.serial.close()
             elif user_choice == 2:
                 set_low_voltage_alarm_threshold(instrument)
                 print("Low Voltage Alarm Threshold changed successfully.")
+                instrument.serial.close()
             elif user_choice == 3:
                 new_address = set_slave_address(instrument)
+                instrument.serial.close()
                 if new_address != SLAVE_ADDRESS:
                     print("Slave Address changed successfully.")
                     instrument = connect_modbus_device(DEVICE_PORT, DEVICE_BAUDRATE, DEVICE_PARITY, DEVICE_STOPBITS)
@@ -83,15 +89,27 @@ def main():
             elif user_choice == 4:
                 set_current_range(instrument)
                 print("Current Range changed successfully.")
+                instrument.serial.close()
             elif user_choice == 5:
                 reset_energy(instrument)
+                instrument.serial.close()
             elif user_choice == 6:
                 break
             else:
                 print("Invalid choice. Please try again.")
-        finally:
-            time.sleep(1)
-            instrument.serial.close()
-            
+                instrument.serial.close()
+        except minimalmodbus.NoResponseError as e:
+            print(f"Error: {e}")
+            print("Please make sure the device is connected and the correct port is specified.")
+            break
+        
+        except Exception as e:
+            print(f"Error: {e}")
+            break
+        
+    if instrument.serial.is_open:
+        instrument.serial.close()
+        
 if __name__ == "__main__":
     main()
+    
