fa07521d66b56801b5451878e90cddd96596d718
croutonso <52742396+croutonso@users.noreply.github.com>
2023-05-02 20:06:53 -0700

    Update pzem_reader.py

--- pzem_reader.py
+++ pzem_reader.py
@@ -1,41 +1,39 @@
 import minimalmodbus
 import serial
+from contextlib import closing
 
 DEVICE_ADDRESS = 0x01
 BAUD_RATE = 9600
 TIMEOUT = 1
 PORT = '/dev/ttyUSB0'
 
+from contextlib import closing
+
 def read_pzem_data():
-    instrument = minimalmodbus.Instrument(PORT, DEVICE_ADDRESS)
-    instrument.serial.baudrate = BAUD_RATE
-    instrument.serial.stopbits = 2
-    instrument.serial.timeout = TIMEOUT
-    instrument.mode = minimalmodbus.MODE_RTU
-
-    # Read the data from the PZEM registers
-    try:
-        voltage = instrument.read_register(0x0000, number_of_decimals=2)
-        current = instrument.read_register(0x0001, number_of_decimals=2)
-        power = instrument.read_register(0x0002, number_of_decimals=1, functioncode=4)
-        energy = instrument.read_register(0x0004, number_of_decimals=0, functioncode=4)
-        power_low = instrument.read_register(0x0002)
-        power_high = instrument.read_register(0x0003)
-        energy_low = instrument.read_register(0x0004)
-        energy_high = instrument.read_register(0x0005)
-        high_voltage_alarm = instrument.read_register(0x0006)
-        low_voltage_alarm = instrument.read_register(0x0007)
-
-        # Print the output
-        print("Voltage: {:.2f} V".format(voltage))
-        print("Current: {:.2f} A".format(current))
-        print("Power: {:.1f} W".format(power * 0.1))
-        print("Energy: {} Wh".format(energy))
-        print("High Voltage Alarm: {}".format("Alarm" if high_voltage_alarm == 0xFFFF else "Not Alarm"))
-        print("Low Voltage Alarm: {}".format("Alarm" if low_voltage_alarm == 0xFFFF else "Not Alarm"))
-    
-    except minimalmodbus.ModbusException as e:
-        print("Error: {}".format(e))
+    with closing(minimalmodbus.Instrument(PORT, DEVICE_ADDRESS)) as instrument:
+        instrument.serial.baudrate = 9600
+        instrument.serial.bytesize = 8
+        instrument.serial.parity = serial.PARITY_NONE
+        instrument.serial.stopbits = 2
+        instrument.serial.timeout = 1
+        
+        try:
+            voltage = instrument.read_register(0x0000, number_of_decimals=2, functioncode=4)
+            current = instrument.read_register(0x0001, number_of_decimals=2, functioncode=4)
+            power_low = instrument.read_register(0x0002, functioncode=4)
+            power_high = instrument.read_register(0x0003, functioncode=4)
+            power = (power_high << 16) + power_low
+            energy_low = instrument.read_register(0x0004, functioncode=4)
+            energy_high = instrument.read_register(0x0005, functioncode=4)
+            energy = (energy_high << 16) + energy_low
+            
+            print(f"Voltage: {voltage} V")
+            print(f"Current: {current} A")
+            print(f"Power: {power * 0.1} W")
+            print(f"Energy: {energy} Wh")
+            
+        except minimalmodbus.IllegalRequestError as e:
+            print(f"Error: {e}")
 
 if __name__ == "__main__":
     read_pzem_data()
