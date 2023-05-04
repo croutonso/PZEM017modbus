d04778cb5e3104d3b33e7726192cefcf00d7d380
croutonso <52742396+croutonso@users.noreply.github.com>
2023-05-03 22:09:21 -0700

    Update pzem_reader.py

--- pzem_reader.py
+++ pzem_reader.py
@@ -7,7 +7,9 @@ DEVICE_ADDRESS = 0x01
 BAUD_RATE = 9600
 TIMEOUT = 1
 PORT = '/dev/ttyUSB0'
+
 def read_pzem_data():
+    # Initialize the connection to the PZEM device
     instrument = minimalmodbus.Instrument(PORT, DEVICE_ADDRESS)
     instrument.serial.baudrate = 9600
     instrument.serial.bytesize = 8
@@ -16,6 +18,7 @@ def read_pzem_data():
     instrument.serial.timeout = 1
     
     try:
+        # Read measurement data
         voltage = instrument.read_register(0x0000, number_of_decimals=2, functioncode=4)
         current = instrument.read_register(0x0001, number_of_decimals=2, functioncode=4)
         power_low = instrument.read_register(0x0002, functioncode=4)
@@ -25,10 +28,18 @@ def read_pzem_data():
         energy_high = instrument.read_register(0x0005, functioncode=4)
         energy = (energy_high << 16) + energy_low
         
+        # Read alarm status
+        high_voltage_alarm = instrument.read_register(0x0006, functioncode=4)
+        low_voltage_alarm = instrument.read_register(0x0007, functioncode=4)
+
         print(f"Voltage: {voltage} V")
         print(f"Current: {current} A")
         print(f"Power: {power * 0.1} W")
         print(f"Energy: {energy} Wh")
+        
+        # Print alarm statuses
+        print(f"High Voltage Alarm: {'Alarm' if high_voltage_alarm == 0xFFFF else 'Clear'}")
+        print(f"Low Voltage Alarm: {'Alarm' if low_voltage_alarm == 0xFFFF else 'Clear'}")
         
     except minimalmodbus.IllegalRequestError as e:
         print(f"Error: {e}")
